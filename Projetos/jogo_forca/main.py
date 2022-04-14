81ed26023ffa4392f41a81a9c74f0ea62cc8213f
chrystiancp95 <65790875+chrystiancp95@users.noreply.github.com>
2022-04-14 16:44:35 -0300

    1° Working Version
    The game is very rudimentar, but it is working.
    Changelog:
    - Added the guess word checker.

--- Projetos/jogo_forca/main.py
+++ Projetos/jogo_forca/main.py
@@ -1,12 +1,22 @@
 secret_word = 'test'
 guess_word = ['_' for char in secret_word]
+used_word = []
+game = True
+life = 3
 
-while True:
+while game:
+    if life == 0:
+        print('Você perdeu!')
+        break
     guess_char = input('Digite: ')
-    for index, char in enumerate(secret_word):
-        if guess_char == char:
-            guess_word[index] = guess_char
-
+    if guess_char in used_word:
+        print('Letra já utilizada!')
+        continue
+    if guess_char in secret_word:
+        for index, char in enumerate(secret_word):
+            if guess_char == char:
+                guess_word[index] = guess_char
+    else:
+        life -= 1
+    used_word.append(guess_char)
     print(''.join([str(char) for char in guess_word]))
-
-    print('Teste Git')
