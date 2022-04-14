28a86260cb64e23f160a32f9c2416e6cc37427a8
chrystiancp95 <65790875+chrystiancp95@users.noreply.github.com>
2022-04-14 16:15:11 -0300

    First Steps
    Creating a guess the word game as a challenge. This is the first step.

---
+++ Projetos/jogo_forca/main.py
@@ -0,0 +1,11 @@
+secret_word = 'test'
+guess_word = ['_' for char in secret_word]
+
+while True:
+    guess_char = input('Digite: ')
+    for index, char in enumerate(secret_word):
+        if guess_char == char:
+            guess_word[index] = guess_char
+
+    print(''.join([str(char) for char in guess_word]))
+
